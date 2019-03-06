c0bcef06b95301d2ea10d872bd3df4fe24b4d933
mishu <mishu@xtelecom.ro>
2019-03-06 14:16:51 +0200

    fixes form import

--- src/bulma/addresses/AddressForm.vue
+++ src/bulma/addresses/AddressForm.vue
@@ -23,7 +23,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 import { Modal } from '@enso-ui/modal/bulma';
-import EnsoForm from '@enso-ui/forms/bulma';
+import { EnsoForm } from '@enso-ui/forms/bulma';
 
 library.add(faLocationArrow);
 
