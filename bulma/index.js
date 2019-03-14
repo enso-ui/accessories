e645d04f8ea1fbbc9d4924dabd4eeee25f860bf1
Mihai <mihai.ocneanu@gmail.com>
2019-03-14 16:02:55 +0200

    added RoAddresses to the bulma export list

--- bulma/index.js
+++ bulma/index.js
@@ -1,6 +1,7 @@
 import Accessories from '../src/bulma/Accessories.vue';
 import AddressesCard from '../src/bulma/addresses/AddressesCard.vue';
 import Addresses from '../src/bulma/addresses/Addresses.vue';
+import RoAddresses from '../src/bulma/addresses/RoAddresses.vue';
 import CommentsCard from '../src/bulma/comments/CommentsCard.vue';
 import Comments from '../src/bulma/comments/Comments.vue';
 import Discussions from '../src/bulma/discussions/Discussions.vue';
@@ -8,6 +9,6 @@ import DocumentsCard from '../src/bulma/documents/DocumentsCard.vue';
 import Documents from '../src/bulma/documents/Documents.vue';
 
 export {
-    Accessories, AddressesCard, Addresses, CommentsCard,
+    Accessories, AddressesCard, Addresses, RoAddresses, CommentsCard,
     Comments, Discussions, DocumentsCard, Documents,
 };
